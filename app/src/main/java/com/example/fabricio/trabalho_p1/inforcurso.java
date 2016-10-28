de823754f1eb2963d9ca205ff0251f865ae113e4
Bruno Penha <cm.java.androd@gmail.com>
2016-10-28 00:39:41 -0200

    Correções necessários para que o projeta possa ser utilizado.

--- app/src/main/java/com/example/fabricio/trabalho_p1/inforcurso.java
+++ app/src/main/java/com/example/fabricio/trabalho_p1/inforcurso.java
@@ -1,21 +1,32 @@
 package com.example.fabricio.trabalho_p1;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
-public class infocurso extends DebugActivity {
+public class Inforcurso extends Activity {
+
+    Button botaoMudarCadastro;
+    Button botaoMudarProgresso;
+    Button botaoSair;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_infocurso);
+
+        botaoMudarCadastro = (Button) findViewById(R.id.btnCadastro);
+        botaoMudarProgresso = (Button) findViewById(R.id.btnProgresso);
+        botaoSair = (Button) findViewById(R.id.btnSair);
 
         botaoMudarCadastro.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
 
-                Intent trocadorTela = new Intent(infocurso.this, cadastro.class);
+                Intent trocadorTela = new Intent(Inforcurso.this, Cadastro.class);
                 startActivity(trocadorTela);
 
             }
@@ -28,11 +39,19 @@ public class infocurso extends DebugActivity {
             @Override
             public void onClick(View v) {
 
-                Intent trocadorTela = new Intent(infocurso.this, progresso.class);
+                Intent trocadorTela = new Intent(Inforcurso.this, Progresso.class);
                 startActivity(trocadorTela);
 
             }
 
+        });
+
+        botaoSair.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent sair = new Intent(Inforcurso.this,LoginActivity.class);
+                startActivity(sair);
+            }
         });
 
     }
